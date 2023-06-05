71934462beef0c20090f7fbce441a2d5664030f0
Максим Ласточкин <mifswallow@mail.ru>
2023-06-05 18:03:37 +0300

    4th commit

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -64,12 +64,12 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 DATABASES = {
    'default': {
-       'ENGINE': os.environ.get('DB_ENGINE', default='django.db.backends.postgresql'),
-       'NAME': os.environ.get('POSTGRES_DB', default='postgres'),
-       'USER': os.environ.get('POSTGRES_USER', default='postgres'),
-       'PASSWORD': os.environ.get('POSTGRES_PASSWORD', default='postgres'),
-       'HOST': os.environ.get('DB_HOST', default='db'),
-       'PORT': os.environ.get('DB_PORT', default='5432'),
+       'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
+       'NAME': os.getenv('POSTGRES_DB', default='postgres'),
+       'USER': os.getenv('POSTGRES_USER', default='postgres'),
+       'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
+       'HOST': os.getenv('DB_HOST', default='db'),
+       'PORT': os.getenv('DB_PORT', default='5432'),
    }
 }
 
